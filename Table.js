de5b49778721d591a68427431a937906528838e9
Abdwarwar <abdelsalam.warwar@hotmail.com>
2024-11-24 09:33:56 +0300

    Update Table.js

--- Table.js
+++ Table.js
@@ -61,7 +61,7 @@
         <thead>
           <tr>
             ${dimensions.map((dim) => `<th>${dim.description || dim.id}</th>`).join("")}
-            ${measures.map((measure) => `<th>${measure.description || measure.id}</th>`).join("")}
+            ${measures.map((measure) => `<th>${measure.id || measure.id}</th>`).join("")}
           </tr>
         </thead>
         <tbody>
